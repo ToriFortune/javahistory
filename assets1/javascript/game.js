b57dc50917b615863a1b3d0d11697216a7ab6f1a
Tori Fortune <toriolley@gmail.com>
2019-03-12 18:19:01 -0500

    Renaming some things

---
+++ assets1/javascript/game.js
@@ -0,0 +1,53 @@
+//The object of the game is to guess the correct answers to the questions asked. If key words are entered as answers, the user gets one point. 
+//Welcome messages
+alert("Welcome!");
+alert("Let's have some fun while we learn about African American history!");
+
+
+
+
+//Ten questions 
+var firstAfricanAmericanPresident = prompt( "Who was the first biracial president of the United Stated?");
+alert (firstAfricanAmericanPresident)
+if (firstAfricanAmericanPresident === "Barrack Obama") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var firstAfricanAmericanGeneral = prompt("Who was the first African American General in the United States?");
+if (firstAfricanAmericanGeneral=== "Benjamin Oliver Davis Sr" || firstAfricanAmericanGeneral === "Benjamin Davis") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var firstAfricanAmericanMillionaire = prompt("Who was the first African American Millionaire?");
+if (firstAfricanAmericanMillionaire=== "CJ Walker") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var culturalRaggaeIcon = prompt("What famous raggae icon uttered the last words 'money cannot buy life?'");
+if (firstAfricanAmericanMillionaire=== "Bob Marley") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+
+var bestSellingNigerianNovelist = prompt("What Nigerian author wrote the best seller 'Things fall apart?'");
+if (firstAfricanAmericanMillionaire=== "Professor Chinua Achebe") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var greatestBoxerOfThe60s = prompt("Name the legendary boxer also nicknamed 'the greatest of all times'");
+if (firstAfricanAmericanMillionaire=== "Mohammed Ali") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var fromPrisonerToPresident = prompt("What former prisoner later became the president of his country as a septuagenarian?");
+if (firstAfricanAmericanMillionaire=== "Dr. Nelson Mandela") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var wealthiestBlackPersonInTheWorld = prompt("Who is the wealthiest black man in the world?");
+if (firstAfricanAmericanMillionaire=== "Aliko Dangote") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var wealthiestPersonEver = prompt("Who is the wealthiest person to ever live?");
+if (firstAfricanAmericanMillionaire=== "Mansa Musa ") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+var willSmithsWife = prompt("What is Will Smith's wife's middle name?");
+if (firstAfricanAmericanMillionaire=== "Koren") {alert ("Good job!"); 
+}
+else{ alert ("Try again")}
+
+console.log ("Game dot script file");
